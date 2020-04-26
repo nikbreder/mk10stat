d24956d66c7d643d71790083546c32f5d8ea080a
nikbreder <64217083+nikbreder@users.noreply.github.com>
2020-04-26 17:23:25 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ def cleanPers(Perk):
 #*************************КОНЕЦ ФУНКЦИЯМ**************************************************
 
 #, parse_mode= 'Markdown'
-bot.send_message(chat_id= chat_id, text="START", parse_mode= '')
+bot.send_message(chat_id= chat_id, text="START", parse_mode= 'Markdown')
 i=1
 while i<500:
     
